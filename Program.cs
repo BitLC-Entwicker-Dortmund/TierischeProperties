15fd06aa7534f5495f14cbcc632965111609b415
k s <klaus.stolz@web.de>
2022-11-22 09:42:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TierischeProperties {
+    class Program {
+        static void Main ( string [ ] args ) {
+            Tier einTier = new Tier ( );
+            einTier.Name = "Sven";
+            einTier.KampfName = "Tiger";
+
+            einTier.SetZweiterNachname ( "the Debile");            
+
+            Console.ReadLine ( );
+        }
+    }
+}
---
+++ Tier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TierischeProperties {
+    class Tier {
+        // java-like
+
+        private string zweiterNachname;
+ 
+        public void SetZweiterNachname (string s ) {
+            zweiterNachname = s;
+        }
+
+        public string GetZweiterNachname ( ) {
+            return zweiterNachname;
+        }
+
+        // c#
+        public string Name { get; set; }
+
+        private string kampfName;
+
+        public string KampfName {
+            get { 
+                return kampfName; }
+            set { kampfName = value; }
+        }
+
+    }
+}
